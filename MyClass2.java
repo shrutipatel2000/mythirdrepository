392011ca61143193808351d8b3c8563c1b261fe5
shruti patel <Shrutipatel203@gmail.com>
2023-10-29 22:05:33 +0000

    mythirdcommit

---
+++ MyClass2.java
@@ -0,0 +1,110 @@
+package mypackage2;
+
+public class MyClass2 {
+    public static void main(String[] args) {
+
+        System.out.println("what is software testing?");
+
+        System.out.println("who dose testing?");
+
+        System.out.println("Qualification of tester");
+
+        System.out.println("challenges of tester");
+
+        System.out.println("what is manual testing?");
+
+        System.out.println("software testing life cycle(STLC)");
+
+        System.out.println("software development life cycle(SDLC)");
+
+        System.out.println("challenges with manual testing");
+
+        System.out.println("7 principals of software testing");
+
+        System.out.println("what is test plan?");
+
+        System.out.println("Why is it important to have test plan in software testing?");
+
+        System.out.println("why do we write test scenario?");
+
+        System.out.println("why do we write test cases?");
+
+        System.out.println("what is RTM?");
+
+        System.out.println("software testing techniques");
+
+        System.out.println("what is equivalence class partitioning?");
+
+        System.out.println("what is boundary value analysis");
+
+        System.out.println("what is error guessing?");
+
+        System.out.println("what is bug?");
+
+        System.out.println(" why are there bugs in the software?");
+
+        System.out.println(" bug life cycle");
+
+        System.out.println(" what is severity?");
+
+        System.out.println("what is priority?");
+
+        System.out.println("what is black box and white box testing?");
+
+        System.out.println("what is level testing?;");
+
+        System.out.println("what is system testing?");
+
+        System.out.println("what is functional and non-functional testing?");
+
+        System.out.println("what is conformation and maintenance testing?");
+
+        System.out.println("what is regression testing and re-testing?");
+
+        System.out.println("what is user acceptance testing?");
+
+        System.out.println("what is alpha testing and beta testing?");
+
+        System.out.println("what is ststic and dynamic testing?");
+
+        System.out.println("what is exploratory testing?");
+
+        System.out.println("what is adhoc testing?");
+
+        System.out.println("which are the software development models?");
+
+        System.out.println("waterfall model");
+
+        System.out.println("iterative model");
+
+        System.out.println("v-model");
+
+        System.out.println("what is verification and validation?");
+
+        System.out.println("agile model");
+
+        System.out.println("what is the 4 agile values?");
+
+        System.out.println("what is the scrum terminology?");
+
+        System.out.println("Agile meetings");
+
+        System.out.println("Fibonacci series");
+
+        System.out.println("Advantages and disadvantages of agile scrum");
+
+        System.out.println("what is defination of done?");
+
+        System.out.println("what is test driven development?");
+
+        System.out.println("what is behaviour driven development?");
+
+        System.out.println("benefits of BDD");
+
+        System.out.println("BDD tools");
+
+        System.out.println("what is gherkin?");
+
+        System.out.println("BDD keywords and examples ");}
+    }
+
